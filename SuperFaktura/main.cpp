8852e3ed9f5b021463f5963b583428e98bbfda2d
macko-manjaro <krupka.maciej@icloud.com>
2020-04-04 06:47:09 +0200

    cout

--- SuperFaktura/main.cpp
+++ SuperFaktura/main.cpp
@@ -12,11 +12,13 @@ class Item
 private:
     std::string nazwa;
     double netto;
+    char stawka_;
     Vat stawka;
     int il;
 
 public:
-    Item(std::string arg_nazwa, double arg_netto, Vat arg_stawka, int arg_il);
+    void set_stawka(const char &stawka_b);
+    Item(std::string arg_nazwa, double arg_netto, char arg_stawka, int arg_il);
 };
 class Invoice
 {
@@ -29,11 +31,25 @@ public:
     void add_item(Item it);
     Invoice(float w_nip, float n_nip);
 };
-Item::Item(std::string arg_nazwa, double arg_netto, Vat arg_stawka, int arg_il)
+void Item::set_stawka(const char &stawka_){
+    switch (stawka_)
+    {
+    case 'A':
+        stawka = Vat::A;
+        break;
+    case 'B':
+        stawka = Vat::B;
+        break;
+    case 'C':
+        stawka = Vat::C;
+        break;
+    }
+}
+Item::Item(std::string arg_nazwa, double arg_netto, char arg_stawka, int arg_il)
 {
     nazwa = arg_nazwa;
     netto = arg_netto;
-    stawka = arg_stawka;
+    set_stawka(arg_stawka);
     il = arg_il;
 }
 Invoice::Invoice(float w_nip, float n_nip)
@@ -41,12 +57,17 @@ Invoice::Invoice(float w_nip, float n_nip)
     w_nip_ = w_nip;
     n_nip_ = n_nip;
 }
+void Invoice::add_item(Item it){
+    items.push_back(it);
+}
 
 int main()
 {
     Invoice inv(7770003699, 1234567890);
-    //inv.add_item(Item("Sruba M3", 0.37, 'A', 100));
-    //inv.add_item(Item("Wiertlo 2 mm", 2.54, 'B', 2));
+    inv.add_item(Item("Sruba M3", 0.37, 'A', 100));
+    inv.add_item(Item("Wiertlo 2 mm", 2.54, 'B', 2));
+    std::cout << std::string(19 , '-') << "FAKTURA VAT" << std::string(19, '-') << std::endl;
+    std::cout << std::string(49 , '=') << std::endl;
     //cout << inv << endl;
     return 0;
 }
