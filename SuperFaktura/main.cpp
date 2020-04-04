3fc1bf888d6b272607e69572e67666b1dc4e3b54
macko-manjaro <krupka.maciej@icloud.com>
2020-04-04 06:06:17 +0200

    klasy

--- SuperFaktura/main.cpp
+++ SuperFaktura/main.cpp
@@ -1,9 +1,52 @@
 #include <iostream>
+#include <vector>
 
-using namespace std;
+enum Vat
+{
+    A = 23,
+    B = 8,
+    C = 0
+};
+class Item
+{
+private:
+    std::string nazwa;
+    double netto;
+    Vat stawka;
+    int il;
+
+public:
+    Item(std::string arg_nazwa, double arg_netto, Vat arg_stawka, int arg_il);
+};
+class Invoice
+{
+private:
+    float w_nip_;
+    float n_nip_;
+    std::vector<Item> items;
+
+public:
+    void add_item(Item it);
+    Invoice(float w_nip, float n_nip);
+};
+Item::Item(std::string arg_nazwa, double arg_netto, Vat arg_stawka, int arg_il)
+{
+    nazwa = arg_nazwa;
+    netto = arg_netto;
+    stawka = arg_stawka;
+    il = arg_il;
+}
+Invoice::Invoice(float w_nip, float n_nip)
+{
+    w_nip_ = w_nip;
+    n_nip_ = n_nip;
+}
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    Invoice inv(7770003699, 1234567890);
+    //inv.add_item(Item("Sruba M3", 0.37, 'A', 100));
+    //inv.add_item(Item("Wiertlo 2 mm", 2.54, 'B', 2));
+    //cout << inv << endl;
     return 0;
 }
