1d82e5cb8ab66974efeb5faae5fe2cc3d5499843
macko-manjaro <krupka.maciej@icloud.com>
2020-04-05 07:35:12 +0200

    operatory add

--- SuperFaktura/main.cpp
+++ SuperFaktura/main.cpp
@@ -22,7 +22,7 @@ public:
     void
     set_stawka(const char &stawka_b);
     Item(std::string arg_nazwa, double arg_netto, char arg_stawka, int arg_il);
-    friend std::ostream &operator<<(std::ostream &ouy, const std::vector<Item> items);
+    friend std::ostream &operator<<(std::ostream &out, const std::vector<Item> items);
 };
 class Invoice
 {
@@ -33,19 +33,32 @@ private:
 
 public:
     void add_item(Item it);
+    long sprzedawca() const;
+    long nabywca()const;
+    Invoice();
     Invoice(long w_nip, long n_nip);
-    friend std::ostream &operator<<(std::ostream &ouy, const std::vector<Item> items);
-    friend std::ostream &operator<<(std::ostream &out, const Invoice &el);
+    bool add(const Invoice &r1);
+    bool add_item(const std::vector<Item> &other_items);
+    //Invoice operator+(const Invoice &r1);
+    friend std::ostream &operator<<(std::ostream &out, const std::vector<Item> items);
+    friend std::ostream &operator<<(std::ostream &out, const Invoice &el); 
 };
-inline double round( double val )
-{
-    if( val < 0 ) return ceil(val - 0.5);
-    return floor(val + 0.5);
+bool Invoice::add(const Invoice &other){
+    if( (sprzedawca() == other.sprzedawca())&&(nabywca() == other.nabywca()) ){
+        return true;
+    }
+    return false;
 }
+bool Invoice::add_item(const std::vector<Item> &other_items){
+    for( Item ot : other_items){
 
+        return true;
+    }
+    return false;
+}
 void Item::set_stawka(const char &stawka_)
 {
-    switch (stawka_)
+    switch (stawka_)    
     {
     case 'A':
         stawka = Vat::A;
@@ -57,6 +70,12 @@ void Item::set_stawka(const char &stawka_)
         stawka = Vat::C;
         break;
     }
+}
+long Invoice::sprzedawca()const{
+    return w_nip_;
+}
+long Invoice::nabywca()const{
+    return n_nip_;
 }
 Item::Item(std::string arg_nazwa, double arg_netto, char arg_stawka, int arg_il)
 {
@@ -94,10 +113,13 @@ std::ostream &operator<<(std::ostream &out, const std::vector<Item> items)
         out.width(5);
         out << std::internal << std::to_string(items[i].il) << " |";
         out.width(5);
+        tmp_netto = items[i].netto * items[i].il * (items[i].stawka / 100.0 + 1);
+        tmp_netto = round(tmp_brutto * 100.0) / 100.0;
         out << std::internal << items[i].netto * items[i].il << " |";
         out.width(8);
         out << std::setprecision(4);
         tmp_brutto = items[i].netto * items[i].il * (items[i].stawka / 100.0 + 1);
+        tmp_brutto = round(tmp_brutto * 100.0) / 100.0;
         out << std::internal << tmp_brutto;
         out << std::endl;
     }
@@ -109,6 +131,8 @@ std::ostream &operator<<(std::ostream &out, const std::vector<Item> items)
         tmp_netto += et.netto * et.il;
     }
     out << std::setfill('-') << std::setw(45) << "RAZEM" << std::setfill('-') << std::setw(5) << "\n";
+    tmp_brutto = round(tmp_brutto * 100.0) / 100.0;
+    tmp_netto = round(tmp_netto * 100.0) / 100.0;
     std::string str1 = std::to_string(tmp_brutto);
     std::string str2 = std::to_string(tmp_netto);
     str1.erase(str1.find_last_not_of('0') + 1, std::string::npos);
@@ -153,9 +177,6 @@ int main()
     inv2.add_item(Item("Sruba M3", 0.37, 'A', 100));
     inv2.add_item(Item("Wiertlo 2 mm", 2.54, 'B', 2));
     std::cout << inv2 << std::endl;
-    double value = 42.08;
-    value = round( value * 100.0 ) / 100.0;
-    std::cout << value << std::endl;
-
+    
     return 0;
 }
